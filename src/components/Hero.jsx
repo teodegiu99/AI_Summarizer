9235ffd070274dcc153f74246ccc6be5bc7745a1
Matteo De Giuseppe <matteodegiuseppe@Mac-mini-di-Matteo.local>
2023-09-11 17:29:00 +0200

    AI summarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import {logo} from '../assets';
+
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
+                <button
+                    type="button"
+                    onClick={() => window.open('https://github.com')}
+                    className='black_btn'>
+                    Github
+                </button>
+            </nav>
+            <h1 className='head_text'>Summarize Article with<br className='max-md:hidden'/>
+                <span className='orange_gradient'>OpenAi GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+                Simplify your reading with Summarize, an article summarizer that trasforms
+                lenghty-articles into clear and concise summaries
+            </h2>
+        </header>
+    )
+}
+
+export default Hero
